f86b4ce07389d6a071979cef0c3c58deeee01c14
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-06-17 17:12:24 +0700

    Init layout

---
+++ app/src/main/java/com/linh/recyclerviewdemo/RecyclerViewFragment.kt
@@ -0,0 +1,21 @@
+package com.linh.recyclerviewdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.linh.recyclerviewdemo.databinding.FragmentRecyclerViewBinding
+
+class RecyclerViewFragment : Fragment() {
+    private lateinit var binding : FragmentRecyclerViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRecyclerViewBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
