fc6040ca2428511ff02e206c5ac2cb4d74d055af
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-06-17 17:24:36 +0700

    Add adapter to RecyclerView

--- app/src/main/java/com/linh/recyclerviewdemo/RecyclerViewFragment.kt
+++ app/src/main/java/com/linh/recyclerviewdemo/RecyclerViewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.linh.recyclerviewdemo.databinding.FragmentRecyclerViewBinding
 
 class RecyclerViewFragment : Fragment() {
@@ -17,5 +18,15 @@ class RecyclerViewFragment : Fragment() {
         binding = FragmentRecyclerViewBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val list = listOf("Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine", "Hello", "Hi", "How are you?", "I'm fine")
+        val adapter = RecyclerViewAdapter(list)
+
+        binding.recyclerRecycyclerview.adapter = adapter
+        binding.recyclerRecycyclerview.layoutManager = LinearLayoutManager(requireContext())
     }
 }
